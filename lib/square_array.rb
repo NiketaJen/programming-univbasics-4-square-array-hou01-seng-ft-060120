7addf71156ebbde65c5c995d3e7a9ef5eab246a3
Niketa Jenerette <niketa.jenerette@gmail.com>
2020-05-12 07:03:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
   counter=0 
   new_array= []
   
-  while counter < array.length do
+  while counter < numbers.length do
    new_array.push (numbers[counter]**2) 
 counter += 1
 end
